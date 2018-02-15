9881b01afa764124df0f51b8f37af5f11fc816d2
Gaabicc1 <36281615+Gaabicc1@users.noreply.github.com>
2018-02-15 19:56:59 -0300

    Add files via upload
    del 1 al 3 y el 6

---
+++ jsInstruccionIF (3).js
@@ -0,0 +1,21 @@
+function Mostrar()
+{
+//tomo la edad  
+	var edad;
+
+	edad = document.getElementById('edad').value;
+
+	if(edad >= 18)
+	{
+		alert("es mayor de edad");
+	}	
+	else 
+	{
+	if (edad <=17)
+	{
+		alert("es menor de edad");
+	}	
+
+	} 
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (7).js
@@ -0,0 +1,16 @@
+function Mostrar()
+{
+//tomo la edad  
+	var edad;
+	var estadocivil;
+
+	edad = document.getElementById('edad').value;
+	estadocivil = document.getElementById('estadoCivil').value;
+
+	if(edad<18)
+	{
+		alert("es muy pequeño para para No ser soltero");
+	}
+
+}//FIN DE LA FUNCIÓN
+
